7d953a01845cfa851443c07b379c0ea0ee5181da
Hussein Hassan <hussein.h.tech@gmail.com>
2023-05-01 08:07:32 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -4,17 +4,13 @@ import https = require("https");
 
 admin.initializeApp();
 
-// run every n minutes and call the api in our app to check for payments deadlines
-export const check_payment_deadline = functions.pubsub
-  .topic("check_payment_deadline")
+export const google_cron_job = functions.pubsub
+  .topic("google_cron_job")
   .onPublish(async () => {
     return new Promise((resolve, reject) => {
-      const request = https.get(
-        `https://system.viandesmetropolitain.ca/api/orders/batch-cron-job`,
-        (res: any) => {
-          res.on("end", resolve);
-        }
-      );
+      const request = https.get(`your api route`, (res: any) => {
+        res.on("end", resolve);
+      });
       request.on("error", reject);
     });
   });
